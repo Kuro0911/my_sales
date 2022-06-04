dba41e8d6eb70418dd128df6914325c96f462d7e
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-06-04 16:45:34 +0530

    init

---
+++ src/components/card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CardWrapper from './Card.style'
+function Card() {
+  return (
+      <CardWrapper>
+          <div>Card</div>
+      </CardWrapper>
+  )
+}
+
+export default Card
---
+++ src/components/card/Card.style.js
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+
+const CardWrapper = styled.div``;
+export default CardWrapper;
